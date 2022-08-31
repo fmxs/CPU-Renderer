f11947a8d2f71857540f7a94c3e9e6aca5242153
Murray <65701532+fmxs@users.noreply.github.com>
2022-08-31 11:14:48 +0800

    Add files via upload

---
+++ Step7/GetInputChar_Example.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <conio.h>//	_kbhit()需要
+//	_getche()	获取输入字符，并回显
+//	_getch()	获取输入字符，不回显
+using namespace std;
+/// <summary>
+/// 获取键盘输入的示例
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	while (true){
+		if (_kbhit())//非阻塞获取用户输入
+		{
+			char cTake = _getch();
+			switch (cTake)
+			{
+			case 'w':
+			case 'W':
+				cout << "你按下了W键!" << endl;
+				break;
+			case 's':
+			case 'S':
+				cout << "你按下了S键!" << endl;
+			case 'a':
+			case 'A':
+				cout << "你按下了A键!" << endl;
+			case 'd':
+			case 'D':
+				cout << "你按下了D键!" << endl;
+			default:
+				break;
+			}
+		}
+	}
+	return 0;
+}
