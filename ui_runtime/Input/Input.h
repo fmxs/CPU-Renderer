882d537c00e26b76cbb2c93fb16d643a0582b1a1
Murray Fang <65701532+fmxs@users.noreply.github.com>
2025-09-10 00:03:08 +0800

    Update Input.h

--- ui_runtime/Input/Input.h
+++ ui_runtime/Input/Input.h
@@ -7,10 +7,9 @@ namespace Input {
     struct State {
       double mouseX=0, mouseY=0;
       bool   lmb=false, rmb=false, mmb=false;
-      int    wheel=0;// 鼠标滚轮滚动量 往上为正，往下为负
+      int    wheel=0;
     };
     void SetupCallbacks(GLFWwindow* win);
     const State& Get();
-    // 鼠标滚动是某一帧的输入，不清理的话下一帧就会残留，导致误以为还在滚动。通常渲染循环结束后手动重置为0
     void ClearFrameWheel();
 }
